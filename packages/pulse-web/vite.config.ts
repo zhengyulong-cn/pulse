ce91ad3a41c42e427fec25170ad065954b17cf38
zhengyulong-cn <zhengyulong.cn@outlook.com>
2026-09-09 22:05:13 +0800

    🎈 perf(pulse-server): 修改pulse-server端口

--- packages/pulse-web/vite.config.ts
+++ packages/pulse-web/vite.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api\/market-data/, ''),
       },
       '/api/server': {
-        target: 'http://127.0.0.1:8080',
+        target: 'http://127.0.0.1:8888',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/server/, ''),
       },
